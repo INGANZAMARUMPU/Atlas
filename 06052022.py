4927b731e596708a6925e8bdf72291488fb7d1ba
INGANZAMARUMPU <jonkur96@gmail.com>
2022-05-07 13:58:59 +0200

    multi compile last algo

--- 06052022.py
+++ 06052022.py
@@ -1,15 +1,16 @@
 from datetime import date, timedelta
 
 last_tot = 0
-with open("inputs/06052022.txt") as file:
-	f_founds = open("output/06052022_results.txt", "w")
-	lines = file.readlines()
-	i = 1
-	base = date(1800, 1, 1)
-	while i < len(lines):
-		str_date = f"{base.day}/{base.month}/{base.year}"
-		line = str_date+","+",".join(lines[i-1].replace("\n", "").replace("\t", ",").split(";"))
-		print(line, file=f_founds)
-		base += timedelta(days=1)
-		i += 1
-	f_founds.close()
+for a in [1, 3, 7, 8, 9]:
+	with open(f"inputs/Groupe {a}.txt") as file:
+		f_founds = open(f"output/Groupe {a}_results.txt", "w")
+		lines = file.readlines()
+		i = 1
+		base = date(1800, 1, 1)
+		while i < len(lines):
+			str_date = f"{base.day}/{base.month}/{base.year}"
+			line = str_date+";"+";".join(lines[i-1].replace("\n", "").replace("\t", ";").split(";"))
+			print(line, file=f_founds)
+			base += timedelta(days=1)
+			i += 1
+		f_founds.close()
