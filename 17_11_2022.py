7705c32491f8db62828d196efd9e9b9b909d856c
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-17 22:53:50 +0200

    ancienne position transposée

--- 17_11_2022.py
+++ 17_11_2022.py
@@ -10,16 +10,20 @@ with open("inputs/17_11_2022.csv", "r") as db:
 		list_line = line[:-1].split(";")
 		combinations = list(itertools.combinations(list_line, 2))
 		for combination in combinations:
-			comb = ";".join(combination)
+			comb = "-".join(combination)
 			if comb in dict_combination:
-				dict_combination[comb].append("-".join(list_line))
+				dict_combination[comb]+=1
 			else:
-				dict_combination[comb] = []
+				dict_combination[comb] = 1
 
 	print("writting results")
 	file = open("output/17_11_2022.csv", 'w')
-	for key in tqdm(dict_combination):
-		values = dict_combination[key]
-		str_values = ";".join(values)
-		print(f"{key};{len(values)};{str_values}", file=file)
+	for line in tqdm(lines):
+		list_line = line[:-1].split(";")
+		combinations = list(itertools.combinations(list_line, 2))
+		entire_comb = ""
+		for combination in combinations:
+			comb = "-".join(combination)
+			entire_comb += f"{comb};{dict_combination[comb]};"
+		print(f"{line[:-1]};{entire_comb}", file=file)
 	file.close()
