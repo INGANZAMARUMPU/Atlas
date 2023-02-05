b40c15f59e9e0400e13562aa44e8f826b4f17172
INGANZAMARUMPU <jonkur96@gmail.com>
2023-02-05 20:17:56 +0200

    done

--- 05_02_2023.py
+++ 05_02_2023.py
@@ -27,16 +27,16 @@ class Item:
 		return f"{self.a}_{self.b}_{self.c}_{self.d}_{self.e}"
 
 	def __str__(self):
-		return f"{self.a};{self.b};{self.c};{self.d};{self.e};{self.c_1};{self.c_2};{self.c_3};{self.c_4};{self.c_5}"
+		return f"{self.a},{self.b},{self.c},{self.d},{self.e},{self.c_1},{self.c_2},{self.c_3},{self.c_4},{self.c_5}"
 
 d_1 = dict()
 d_2 = dict()
 d_3 = dict()
 d_4 = dict()
 d_5 = dict()
-db = []
+items = []
 
-title = "A;B;C;D;E;A,AB,ABC,ABCD,ABCDE"
+title = "A,B,C,D,E,A COUNT,AB COUNT,ABC COUNT,ABCD COUNT,ABCDE COUNT"
 
 print("reading sources...")
 with open(f"inputs/05_02_2023.csv") as file:
@@ -47,40 +47,38 @@ with open(f"inputs/05_02_2023.csv") as file:
 		if item.to_1() in d_1:
 			d_1[item.to_1()] += 1
 		else:
-			d_1[item.to_1()] = 0
+			d_1[item.to_1()] = 1
 
 		if item.to_2() in d_2:
 			d_2[item.to_2()] += 1
 		else:
-			d_2[item.to_2()] = 0
+			d_2[item.to_2()] = 1
 
 		if item.to_3() in d_3:
 			d_3[item.to_3()] += 1
 		else:
-			d_3[item.to_3()] = 0
+			d_3[item.to_3()] = 1
 
 		if item.to_4() in d_4:
 			d_4[item.to_4()] += 1
 		else:
-			d_4[item.to_4()] = 0
+			d_4[item.to_4()] = 1
 
 		if item.to_5() in d_5:
 			d_5[item.to_5()] += 1
 		else:
-			d_5[item.to_5()] = 0
+			d_5[item.to_5()] = 1
 
-		db.append(item)
+		items.append(item)
 
-print(len(db))
-print(len(d_1))
-print(len(d_2))
-print(len(d_3))
-print(len(d_4))
-print(len(d_5))
-
-# print("exporting results...")
-# with open(f"output/05_02_2023.csv", "w") as f_founds:
-# 	print(title, file=f_founds)
-# 	for item in tqdm(items):
-# 		print(item, file=f_founds)
+print("exporting results...")
+with open(f"output/05_02_2023.csv", "w") as f_founds:
+	print(title, file=f_founds)
+	for item in tqdm(items):
+		item.c_1 = d_1[item.to_1()]
+		item.c_2 = d_2[item.to_2()]
+		item.c_3 = d_3[item.to_3()]
+		item.c_4 = d_4[item.to_4()]
+		item.c_5 = d_5[item.to_5()]
+		print(item, file=f_founds)
 	
